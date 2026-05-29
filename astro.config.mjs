cd6de1c6dc000a9f7081d1d2d98338e5cffb9983
Misael Abanto <29669824+misaelabanto@users.noreply.github.com>
2026-05-29 15:56:48 -0500

    chore(config): update site URL and remove unused files
    - Changed site URL in astro.config.mjs
    - Removed .pnpm-store from cache paths
    - Deleted wrangler.jsonc file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://orcebot.pages.dev',
+  site: 'https://orcebot.com',
   integrations: [react()],
-  adapter: cloudflare(),
 });
